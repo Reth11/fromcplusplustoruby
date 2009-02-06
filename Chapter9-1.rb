a028df8d8e8759d0ac9ef4ea537f59c9e0cabeb6
Kyle Tuffentsamer <KTuffentsamer@austin.rr.com>
2009-02-06 15:13:41 -0600

    Signed-off-by: Kyle Tuffentsamer <KTuffentsamer@austin.rr.com>

--- Chapter9-1.rb
+++ Chapter9-1.rb
@@ -17,6 +17,78 @@ according to the following rule:
   
   a)Reads exam scores into array scores
   b)Computes average of count student scores
-  c)Pritns a table showing each student's score and grade on a seperate line
+  c)Prints a table showing each student's score and grade on a seperate line
   d)Prints student grade after comparing
 =end
+
+#Read file in...
+def FileRead(line_num, temp_line)
+    #file read into temp_line
+    line_num = 0
+    File.open('Chp9Pgm1.txt','r') do |f1|
+      while line = f1.gets
+        temp_line[line_num] = line
+        line_num = line_num +1
+      end
+    end
+    line_num=line_num-1
+    return line_num, temp_line
+end
+
+#Read exam scores into array scores
+def ReadExamScores(line_num, temp_line, exam_scores)
+    for @line in 0..line_num do
+      exam_scores[@line] = temp_line[@line].to_i
+    end
+end
+
+#Computes average of count students scores
+def ComputeScores(line_num, exam_scores, avg_count)
+    @total=0
+    for @line in 0..line_num do
+      @total = @total + exam_scores[@line]
+    end
+    avg_count = @total / line_num
+    return avg_count
+end
+
+#Print a table showing: Student's score and grade (on a seperate line)
+def PrintScores(line_num, exam_scores)
+    for @line in 0..line_num do
+      print "#{exam_scores[@line]}\t"
+      case exam_scores[@line]
+        when 0..59.99: print "F\n"
+        when 60..69.99: print "D\n"
+        when 70..79.99: print "C\n"
+        when 80..89.99: print "B\n"
+        when 90..100: print "A\n"
+        else print "Problem"
+      end
+    end
+end
+
+#Print student grade after comparing 
+def PrintComparedScores(line_num, exam_scores, avg_count)
+    for @line in 0..line_num do
+      print "#{exam_scores[@line]}\t"
+      case exam_scores[@line]
+        when 0..(avg_count-10.001): print "unsatifactory\n"
+        when (avg_count-10)..(avg_count+10): print "satisfactory\n"
+        when (avg_count+10.001)..100: print "outstanding\n"
+      end
+    end
+end
+
+=begin
+we Start here...
+=end
+temp_line = []
+exam_scores = []
+line_num=0
+avg_count = 0
+line_num, temp_line = FileRead(line_num, temp_line) #reads the file in
+ReadExamScores(line_num, temp_line, exam_scores)
+avg_count = ComputeScores(line_num, exam_scores, avg_count)
+PrintScores(line_num, exam_scores)
+PrintComparedScores(line_num, exam_scores, avg_count)
+puts "The average is #{avg_count}."
